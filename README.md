fbbc6b1fae8dfb2cae652a29f673633d2fa1a60a
Nick Doiron <ndoiron@mapmeld.com>
2016-01-17 17:52:07 +0545

    basic CLI for creating Georeactor mapConfig

---
+++ README.md
@@ -0,0 +1,40 @@
+# georeactor-cli
+
+Georeactor is an experimental interface for creating interactive maps.
+
+This is the command-line version
+
+## Installing
+
+You must have NodeJS and npm installed. Usually installing Node installs npm, too.
+
+```bash
+npm install georeactor-cli -g
+```
+
+Now you're ready to create Georeactor maps from the command line, just by pointing it at files!
+
+## Usage
+
+```bash
+georeactor borders.geojson
+> converting GeoJSON to TopoJSON
+> saving Leaflet + OSM version
+> generating map...
+```
+
+### Options
+
+Very useful options to make different kinds of maps!
+
+```bash
+georeactor borders.json -f GeoJSON -m google -d gmaps
+```
+
+* -f: format of source file (GeoJSON, TopoJSON, KML, and SHP supported)
+* -m: map type (Google and Leaflet supported, defaults to Leaflet)
+* -d: div id (defaults to "map")
+
+## License
+
+MIT license
---
+++ georeactor.js
@@ -0,0 +1,55 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+
+const program = require('commander');
+const toTopoJSON = require('to-topojson').convertFile;
+const knownToTopoJSON = require('to-topojson').convertFileWithFormat;
+
+var mapConfig = {
+  div: "map",
+  data: []
+};
+
+program
+  .version('0.5.0')
+  .arguments('<sourceFile>')
+  .option('-f --format <format>', 'format of source file', /^(GeoJSON|TopoJSON|SHP|KML)$/i, '')
+  .option('-m --map <mapType>', '(Google or Leaflet)', /^(google|leaflet)$/i, 'leaflet')
+  .option('-d --div <divID>', 'id of map container', /(.*)/, 'map')
+  .parse(process.argv);
+
+if (!program.args.length) {
+  console.error('Did not include a file name! Run georeactor --help');
+} else {
+  var sourceFile = program.args[0];
+
+  function next (err) {
+    if (err) {
+      console.error('The conversion process had an error:');
+      console.log(JSON.stringify(err));
+      return;
+    }
+    mapConfig.map = program.map;
+    mapConfig.div = program.div;
+    mapConfig.data.push('mapdata.topojson');
+    saveMapWithConfig(mapConfig);
+  }
+
+  if (program.format) {
+    knownToTopoJSON(sourceFile, program.format, 'mapdata.topojson', next, console.log);
+  } else {
+    toTopoJSON(sourceFile, 'mapdata.topojson', next, console.log);
+  }
+}
+
+function saveMapWithConfig(mapConfig) {
+  console.log(mapConfig);
+  if (mapConfig.map === 'google') {
+    console.log('saving Google Maps API version');
+  } else if (mapConfig.map === 'leaflet') {
+    console.log('saving Leaflet + OSM version');
+  } else {
+    console.log('Must use "google" or "leaflet" - other map viewers not yet supported');
+  }
+}
